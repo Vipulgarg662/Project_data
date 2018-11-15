efb0989da622825e21b70a4d8bd63269b17ee2a8
vipul <vipulgarg662@gmail.com>
2018-11-15 18:29:17 +0530

    Added System commands

--- Voice_bot.py
+++ Voice_bot.py
@@ -1,10 +1,12 @@
+from tkinter import *
 import speech_recognition as sr
 import requests
+import webbrowser
 import os
+import re
 from gtts import gTTS
 
-def Weather_Status():
-    city='Meerut'
+def Weather_Status(city):
     url="http://api.openweathermap.org/data/2.5/weather?q={}&appid=2bca5db701a487480073a7661ff4e481&units=metric".format(city)
     res=requests.get(url)
     data=res.json()
@@ -12,12 +14,9 @@ def Weather_Status():
     print('temperature    :',data['main']['temp'],"C")
     print("humidity       :",data['main']['humidity'])
     print("Weather Status :",data['weather'][0]['description'])
-    print("Wind Speed     :",data['wind']['speed'])
-    t=str(data['main']['temp'])
-    Text_to_speech(t)
+    print("Wind Speed     :",data['wind']['speed'],'m/s')
 
-def Movie_Info():
-    movie=Speech_to_text()
+def Movie_Info(movie):
     url="http://www.omdbapi.com/?t={}&apikey=44115df3".format(movie)
     res=requests.get(url)
     data=res.json()
@@ -31,21 +30,35 @@ def Movie_Info():
     print("Released Date :",data['Released'])
     print("Runtime       :",data['Runtime'])
     print("Awards        :",data['Awards'])
-    print("BoxOffice Coll:",data['Boxoffice'])
+    print("BoxOffice Coll:",data['BoxOffice'])
     print("Country       :",data['Country'])
     print("Imdb Rating   :",data['imdbRating'])
     print("Plot          :",data['Plot'])
     
+def Open_website(a):
+    url="https://www."+a
+    webbrowser.open(url)
+
+def Open_apps(a):
+    os.system("start {}".format(a))
+
+def System_shutdown(a):
+    if a=='shutdown':
+        os.system("shutdown -s")
+    elif a=='restart':
+        os.system("shutdown -r")
+    else:
+        os.system("shutdown -l")
+
 def Speech_to_text():
     mic=sr.Microphone()
     r = sr.Recognizer()
     with mic as source:
         r.adjust_for_ambient_noise(source)
-        print("I can hear you..")
         audio=r.listen(source)
         try:
             text=r.recognize_google(audio)
-            #print(text)
+            print("You said : ",text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
@@ -53,17 +66,50 @@ def Speech_to_text():
         return text
 
 def Text_to_speech(x):
-    language='en'
-    myobj = gTTS(text=x, lang=language, slow=False)
-    myobj.save("output.mp3") 
+    print(x)
+    #language='en'
+    #myobj = gTTS(text=x, lang=language)
+    #myobj.save("output.mp3") 
     #os.system("output.wav")
+    
+def assistant(text):
+    if 'movie' in text:
+        reg=re.search('(.*) (.*)',text)
+        movie=reg.group(2)
+        Movie_Info(movie)
+    elif 'weather' in text:
+        reg=re.search('(.*)weather in (.*)',text)
+        city=reg.group(2)
+        if(city=='my city'):
+            city='meerut'
+        Weather_Status(city)
+    
+    elif 'open app' in text:
+        reg=re.search('open app (.*)',text)
+        app=reg.group(1)
+        Open_apps(app)
+
+    elif 'open website' in text:
+        reg=re.search('open website (.*)',text)
+        website=reg.group(1)
+        Open_website(website)
+
+    elif 'my computer' in text:
+        reg=re.search('(.*) my .*',text)
+        command=reg.group(1)
+        System_shutdown(command)
+
+    elif 'exit' in text:
+        return 'exit'
 
 
-text=Speech_to_text()
-#print(text)
-if text=='Movie':
-    Movie_Info()
-elif text=='weather':
-    Weather_Status()
+#root=Tk()
+while(1):
+    Text_to_speech("Tell me what to do")
+    a=assistant(Speech_to_text())
+    if a=='exit':
+        break
+    
+    
     
 
